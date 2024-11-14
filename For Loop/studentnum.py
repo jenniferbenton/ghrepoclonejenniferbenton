84cfe9ae96a6603c7c48d0931ea957b5060d9238
Jennifer Benton <50045336@belfastmet.ac.uk>
2024-11-14 15:31:05 +0000

    loops

---
+++ For Loop/studentnum.py
@@ -0,0 +1,17 @@
+while True:
+    Student_Number = input("Please enter your student number: ")
+    if Student_Number == "50045336":
+        print("Student number found! Hi Jennifer!")
+        break
+    else:
+        print("Student not found.")
+        break
+
+        while True:
+            password = input("Please enter your password Jennifer: ")
+            if password == "Marlo":
+                print("You are now logged in")
+                break
+            else:
+                print("Wrong Password")
+                break
